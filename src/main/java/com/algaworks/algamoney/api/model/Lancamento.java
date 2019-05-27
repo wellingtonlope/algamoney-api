72961728491ca82a5a8ba76ce71238c6c3dffdaa
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-05-27 09:55:34 -0400

    aula 5.5

--- src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
+++ src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
@@ -1,6 +1,7 @@
 package com.algaworks.algamoney.api.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
@@ -12,18 +13,24 @@ public class Lancamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
+    @NotNull
     private String descricao;
+    @NotNull
     @Column(name = "data_vencimento")
     private LocalDate dataVencimento;
     @Column(name = "data_pagamento")
     private LocalDate dataPagamento;
+    @NotNull
     private BigDecimal valor;
     private String observacao;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TipoLancamento tipo;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_categoria")
     private Categoria categoria;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_pessoa")
     private Pessoa pessoa;
